080d683b611071c73c78befb95069ebad633d905
yaozu <127073530+yaozuting@users.noreply.github.com>
2025-04-30 00:52:11 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/stockAlpha/', // ✅ IMPORTANT for GitHub Pages
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  }
 })
