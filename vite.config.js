88d275b83a9fae9c2ac37eca26b942a321b560c5
yaozu <127073530+yaozuting@users.noreply.github.com>
2025-04-30 01:10:57 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  base: '/stockAlpha/',
+  base: '/stockAlpha/', // For GitHub Pages
   plugins: [react()],
   build: {
     outDir: 'dist',
     rollupOptions: {
       input: {
-        main: 'index.html',
+        main: 'index.html', // Helps ensure fallback routing
       }
     }
   }
